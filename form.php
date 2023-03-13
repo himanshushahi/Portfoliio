a0f8645817d6096a3c46d9980c467cecbe3ba6be
Himanshu Kumar Shahi <113952620+himanshushahi@users.noreply.github.com>
2023-03-13 15:32:35 +0530

    Update form.php

--- form.php
+++ form.php
@@ -1,12 +1,13 @@
 <?php
   $to = "himanshushahi0478@gmail.com";
   $subject = "Form Submission";
+  $name = "Sender Name";
 
   $htmlContent = '<p> <b>Client Name:</b> Himanshu Kumar </p>
    <p> <b>Email:</b> himanshu105@gmail.com</p>
    <p> <b>Massage</b> test-mail</p>';
 
-  $headers = "From:" . $name;
+  $headers = "From:" . $name . "\r\n";
   $headers .= "MIME-Version: 1.0\r\n";
   $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
@@ -18,3 +19,4 @@
     echo "Error: Message was not sent, Try again Later";
   }
 ?>
+
