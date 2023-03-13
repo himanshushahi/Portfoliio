6bbcfa13f8bbc44631d27845d81ea2fd6a4004fd
Himanshu Kumar Shahi <113952620+himanshushahi@users.noreply.github.com>
2023-03-13 15:28:26 +0530

    Update form.php

--- form.php
+++ form.php
@@ -1,26 +1,20 @@
 <?php
-if (isset($_POST['submit'])) {
-  $name = $_POST['Name'];
-  $email = $_POST['Email'];
-  $message = $_POST['massage'];
-
   $to = "himanshushahi0478@gmail.com";
   $subject = "Form Submission";
 
-  $htmlContent = '<p> <b>Client Name:</b> ' . $name . '</p>
-   <p> <b>Email:</b> ' . $email . '</p>
-   <p> <b>Massage</b> ' . $message . '</p>';
+  $htmlContent = '<p> <b>Client Name:</b> Himanshu Kumar </p>
+   <p> <b>Email:</b> himanshu105@gmail.com</p>
+   <p> <b>Massage</b> test-mail</p>';
 
-  $headers = "From:" . $name . "<" . $email . ">";
+  $headers = "From:" . $name;
   $headers .= "MIME-Version: 1.0\r\n";
   $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
   $result = mail($to, $subject, $htmlContent, $headers);
 
   if ($result) {
-    $success = "The message was sent successfully!";
+    echo "The message was sent successfully!";
   } else {
-    $failed = "Error: Message was not sent, Try again Later";
+    echo "Error: Message was not sent, Try again Later";
   }
-}
 ?>
